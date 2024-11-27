116a70d0e55aa9e3f78ca362b6f47cd2cf05e9e3
Beksultan Chomoev <chomoevbeksultan@gmail.com>
2024-11-27 12:21:51 -0600

    Task 1 is done.

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+const port = 8000;
+
+app.get('/', (req, res) => {
+    return res.send('Home Page');
+});
+
+app.get('/:userText', (req, res) => {
+    return res.send(req.params.userText);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
